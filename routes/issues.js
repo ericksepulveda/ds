2a1f9b4ef3d925b9be422d65388355af71e2b8ae
ericksepulveda <erick.sepulveda@alumnos.usm.cl>
2018-12-03 01:56:49 -0300

    fix typo

--- routes/issues.js
+++ routes/issues.js
@@ -19,14 +19,17 @@ router.post('/', async function (req, res, next) {
 router.post('/:id/assign', async function (req, res, next) {
   const { id } = req.params
   const IssuerId = 1
-  const updated = await Issue.update({ IssuerId, status: 'wip' }, { where: { id } })
+  const updated = await Issue.update({ IssuerId, state: 'wip' }, { where: { id } })
   res.send(updated)
 })
 
 router.post('/:id/status', async function (req, res, next) {
   const { id } = req.params
-  const { status } = req.body
-  const updated = await Issue.update({ status }, { where: { id } })
+  const { state } = req.body
+
+  if (!state) return res.send({})
+
+  const updated = await Issue.update({ state }, { where: { id: Number(id) } })
   res.send(updated)
 })
 
